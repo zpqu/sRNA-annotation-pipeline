de0a8eeb4622ac7564014784409982b21869f4db
zpqu <quzp2006@gmail.com>
2026-07-31 15:40:40 +0930

    feat: add all reads size distribution

--- scripts/rScripts/3_figure_annotation_RMlast.R
+++ scripts/rScripts/3_figure_annotation_RMlast.R
@@ -1,71 +1,91 @@
 ##Date: 21/07/2015
 ##Author: Zhipeng
-## This script is used to make plot for size distribution for all small RNA data
+## This script is used to make plots for small RNA annotation results (step 3).
+## It reads the two consolidated count tables produced by step 2:
+##   output/Table2a_annotation_count_unique_reads.csv   (unique reads)
+##   output/Table2b_annotation_count_all_reads.csv      (all reads)
+## and draws annotation barplots for the "read" and the primiRNA/snoRNA/piRNA/
+## tRNA gene-feature classes, showing the unique-reads and all-reads flavors
+## side by side (dodged bars). Counts use a log10 y-axis because the annotation
+## categories span orders of magnitude; a percentage panel is added beneath.
+## Output figures follow the step naming rule: Figure3a, Figure3b, ...
 
 library(ggplot2)
 library(scales)
+library(data.table)
+library(patchwork)
 
-###for all small RNAs
-all.read.df = NULL
-files = list.files(path = "../../output/", pattern = ".annotation.count.txt$")
+###read consolidated count tables from step 2
+unique.tab = read.csv("../../output/Table2a_annotation_count_unique_reads.csv")
+all.tab = read.csv("../../output/Table2b_annotation_count_all_reads.csv")
+unique.tab$flavor = "unique reads"
+all.tab$flavor = "all reads"
+all.df = rbind(unique.tab, all.tab)
 
-for(i in seq(along = files)){
-      file.name = paste("../../output/", files[i], sep = "")
-      class.name = sample.name = files[i]
-      sample.name = gsub("\\.\\w+\\.annotation\\.count\\.txt", "", sample.name)
-      class.name = gsub("\\.annotation\\.count\\.txt", "", class.name)
-      class.name = gsub(".+\\.", "", class.name)
+class.map = c(read = "read.annotation",
+              primiRNA = "primiRNA.annotation",
+              snoRNA = "snoRNA.annotation",
+              piRNA = "piRNA.annotation",
+              tRNA = "tRNA.annotation")
+class.list = names(class.map)
 
-      single.read.df = NULL
-      print(paste("Now is processing ...", sample.name, Sys.time()))
-      single.read.df = read.delim(file.name, header = T)
-      single.read.df$sample = rep(sample.name, nrow(single.read.df))
-      single.read.df$class = rep(class.name, nrow(single.read.df))
-			 
-      single.read.df$per = single.read.df$Freq/sum(single.read.df$Freq)
-      all.read.df = rbind(all.read.df, single.read.df)
-}
+read.levels = c("primiRNA", "snoRNA", "piRNA", "tRNA", "RM", "refGene.NM.exon",
+                "refGene.NM.intron", "lincRNA.exon", "AS.primiRNA", "AS.snoRNA",
+                "AS.piRNA", "AS.tRNA", "AS.RM", "AS.refGene.NM.exon",
+                "AS.refGene.NM.intron", "AS.lincRNA.exon", "other")
+region.levels = c("CDS", "5UTR", "3UTR", "intron", "up1k", "down1k", "RM",
+                  "AS.CDS", "AS.5UTR", "AS.3UTR", "AS.intron", "AS.up1k",
+                  "AS.down1k", "AS.RM", "intergenic")
 
-###
-class.list = c("read", "primiRNA", "piRNA", "snoRNA", "tRNA")
-sample.num = length(unique(all.read.df$sample))
-if(sample.num < 4){
-	      fig.width = sample.num*4
-	      fig.height = 4
-}else{
-	fig.height = (as.integer((sample.num-1)/4)+1)*4
-	fig.width = 16
-}
+rot.theme = theme(axis.text.x = element_text(angle = 45, hjust = 1))
+flavor.cols = c("unique reads" = "#1f78b4", "all reads" = "#e31a1c")
+
 for(j in 1:length(class.list)){
-      single.class.df = all.read.df[all.read.df$class == class.list[j], ]
-      if(class.list[j] == "read"){
-       single.class.df$Var1 = factor(single.class.df$Var1, 
-       			   levels = c("primiRNA", "snoRNA", "piRNA", "tRNA", "RM", "refGene.NM.exon",
-			   "refGene.NM.intron", "lincRNA.exon", "AS.primiRNA", "AS.snoRNA", "AS.piRNA", 
-			   "AS.tRNA", "AS.RM", "AS.refGene.NM.exon",
-			   "AS.refGene.NM.intron", "AS.lincRNA.exon", "other"))
-      }else{
-       single.class.df$Var1 = factor(single.class.df$Var1, 
-       			   levels = c("CDS", "5UTR", "3UTR", "intron", "up1k",
-			   "down1k", "RM", "AS.CDS", "AS.5UTR", "AS.3UTR", 
-			   "AS.intron", "AS.up1k", "AS.down1k", "AS.RM", "intergenic"))	
-      }
+      class = class.list[j]
+      category = class.map[[class]]
+      sub.dt = as.data.table(all.df[all.df$category == category, ])
+      if(nrow(sub.dt) == 0) next
+      sub.dt$item = factor(sub.dt$item, levels = if(class == "read") read.levels else region.levels)
+      sample.num = length(unique(sub.dt$sample))
+      fig.width = max(8, sample.num * 4)
+      fig.height = 8.5
 
-      p.sample.class.barplot = ggplot(data = single.class.df, aes(x = Var1, y = Freq)) +
-		   geom_bar(stat = "identity") +
-		   xlab("") + ylab("Count") +
-		   scale_y_continuous(labels = comma) +		   
-		   theme_bw() +
-		   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-		   facet_wrap(~sample, ncol = 4)
-      ggsave(p.sample.class.barplot, file = paste0("../../plots/Fig1a.", class.list[j], "_annotation_barplot.pdf"), width = fig.width, height = fig.height)
+      ##percentage panel: complete all item x sample x flavor combos (zero-filled)
+      all.combo = expand.grid(sample = unique(sub.dt$sample),
+                              item = levels(sub.dt$item),
+                              flavor = c("unique reads", "all reads"),
+                              stringsAsFactors = FALSE)
+      sub.full = merge(sub.dt[, .(sample, item, flavor, Freq)], all.combo,
+                       by = c("sample", "item", "flavor"), all = TRUE)
+      sub.full$Freq[is.na(sub.full$Freq)] = 0
+      sub.full$item = factor(sub.full$item, levels = levels(sub.dt$item))
+      sub.full = as.data.table(sub.full)
+      sub.full[, per := Freq / sum(Freq), by = .(sample, flavor)]
 
-      p.sample.class.percent.plot = ggplot(data = single.class.df, aes(x = Var1, y = per)) +
-		   geom_bar(stat = "identity") +
-		   xlab("") + ylab("") +
-		   scale_y_continuous(labels = percent) +
-		   theme_bw() +
-		   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-		   facet_wrap(~sample, ncol = 4)
-      ggsave(p.sample.class.percent.plot, file = paste0("../../plots/Fig1b.", class.list[j], "_annotation_barplot.percentage.pdf"), width = fig.width, height = fig.height)
+      p.count = ggplot(sub.dt[Freq > 0], aes(x = item, y = Freq, fill = flavor)) +
+            geom_bar(stat = "identity", position = position_dodge(preserve = "single"), width = 0.7) +
+            scale_y_log10(labels = comma) +
+            scale_fill_manual(values = flavor.cols, name = "Reads") +
+            facet_wrap(~sample, ncol = 4) +
+            labs(title = paste0(class, " annotation - count"),
+                 y = "Count (log10 scale)", x = NULL) +
+            theme_bw() + rot.theme
+
+      p.per = ggplot(sub.full, aes(x = item, y = per, fill = flavor)) +
+            geom_bar(stat = "identity", position = "dodge", width = 0.7) +
+            scale_y_continuous(labels = percent) +
+            scale_fill_manual(values = flavor.cols, name = "Reads") +
+            facet_wrap(~sample, ncol = 4) +
+            labs(title = paste0(class, " annotation - percentage"),
+                 y = "Percentage", x = NULL) +
+            theme_bw() + rot.theme
+
+      fig.base = paste0("../../plots/Figure3", letters[j], ".", class, "_annotation_barplot")
+      pdf(paste0(fig.base, ".pdf"), width = fig.width, height = fig.height)
+      print(p.count / p.per)
+      dev.off()
+      png(paste0(fig.base, ".png"), width = fig.width * 300, height = fig.height * 300, res = 300)
+      print(p.count / p.per)
+      dev.off()
+      print(paste("saved:", fig.base, Sys.time()))
 }
