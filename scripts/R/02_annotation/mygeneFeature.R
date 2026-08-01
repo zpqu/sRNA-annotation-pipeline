cd748606c582c25c15f49e6673a269e59e594d7a
zpqu <quzp2006@gmail.com>
2026-08-01 22:46:18 +0930

    feat: parametrize mygeneFeature sense overlap type
    Add a 'type' argument (default 'within', unchanged behaviour) supporting
    'within' (read fully contained in feature) and 'union' (read within feature
    OR feature within read) for the sense gene-feature annotation.

--- scripts/R/02_annotation/mygeneFeature.R
+++ scripts/R/02_annotation/mygeneFeature.R
@@ -1,4 +1,4 @@
-mygeneFeature = function(bam){
+mygeneFeature = function(bam, type = "within"){
               single.bam.gr = get(bam)
               single.bam.gr$region = NULL
               single.bam.new.gr = test.bam.gr
@@ -6,6 +6,9 @@ mygeneFeature = function(bam){
               single.other.gr = NULL
 
               ##sense
+              ## type = "within": reads fully contained in the feature (default).
+              ## type = "union": either the read is fully contained in the feature
+              ## OR the feature is fully contained in the read (union containment).
               for(m in 1:length(geneFeature.list)){
                     geneFeature.name = geneFeature.list[m]
 		    geneFeature.id = gsub("mm39\\.", "", geneFeature.name)
@@ -15,14 +18,21 @@ mygeneFeature = function(bam){
 
                     single.bam.ol = NULL
                     single.bam.hit.gr = NULL
-                    single.bam.ol = findOverlaps(single.bam.gr, get(geneFeature.name), type = "within")
-                    single.bam.hit.gr = single.bam.gr[unique(queryHits(single.bam.ol))]
+                    if(type == "union"){
+                      ol1 = findOverlaps(single.bam.gr, get(geneFeature.name), type = "within")
+                      ol2 = findOverlaps(get(geneFeature.name), single.bam.gr, type = "within")
+                      hit.ids = unique(c(queryHits(ol1), subjectHits(ol2)))
+                    }else{
+                      single.bam.ol = findOverlaps(single.bam.gr, get(geneFeature.name), type = "within")
+                      hit.ids = unique(queryHits(single.bam.ol))
+                    }
+                    single.bam.hit.gr = single.bam.gr[hit.ids]
                     single.bam.hit.gr$region = rep(geneFeature.id, length(single.bam.hit.gr))
                     single.bam.new.gr = c(single.bam.new.gr, single.bam.hit.gr)
-                    if(length(unique(queryHits(single.bam.ol))) == 0){
+                    if(length(hit.ids) == 0){
                      single.bam.gr = single.bam.gr
                     }else{
-                     single.bam.gr = single.bam.gr[!single.bam.gr$rm.key %in% single.bam.gr$rm.key[unique(queryHits(single.bam.ol))]]
+                     single.bam.gr = single.bam.gr[!single.bam.gr$rm.key %in% single.bam.gr$rm.key[hit.ids]]
                     }
               }
              for(g in 1:length(geneFeature.list)){
